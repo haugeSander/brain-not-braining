857290af51c94890d783c47881e712e07f3e69f1
Giulio Zemignani <g.zemignani@studenti.unimol.it>
2025-12-11 21:13:02 +0100

    feat(puzzle-8): added minimap

---
+++ Assets/Scripts/Gameplay/Level_8/MinmapBehaviour.cs
@@ -0,0 +1,18 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class MinimapBehaviour : MonoBehaviour
+{
+    public Transform PlayerTransform;
+    void Start()
+    {
+        
+    }
+
+    void LateUpdate()
+    {
+        Vector3 newPosition = PlayerTransform.position;
+        newPosition.y = transform.position.y;
+        transform.position = newPosition;
+    }
+}
